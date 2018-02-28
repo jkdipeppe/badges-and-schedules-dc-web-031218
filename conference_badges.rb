18a4fd144cb4f6e0ba34eea6554a8f4a10315e3d
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-02-28 20:14:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end# Write your code here.
 
 def batch_badge_creator(arr)
-  arr.each{|bade_maker(name)| puts badge_maker(name)}
+  arr.each{|badge_maker(name)| puts badge_maker(name)}
 end
 
 def assign_rooms()
